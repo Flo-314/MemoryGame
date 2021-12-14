10c2c7f75bbb238d83852a7d612426dcff72bfb3
flo-314 <julianflo1916@gmail.com>
2021-12-14 12:40:04 -0300

    .

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react';
 function Footer() {
     return ( <footer>
-        Created by FLO
+        Created by FLO <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt = "github icon" onClick={() => window.location.href = 'https://github.com/Flo-314'}></img>
     </footer>);
 }
 
